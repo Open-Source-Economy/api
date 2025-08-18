0e50ef6618aab2aad42d8cdb1119a29cb75359f1
Lauriane Mollier <lauriane@open-source-economy.com>
2025-08-18 11:30:23 +0200

    WIP

---
+++ src/dto/onboarding/projectItems/RemoveDeveloperProjectItem.dto.ts
@@ -0,0 +1,11 @@
+import { DeveloperRoleType, MergeRightsType, Owner, OwnerId, ProjectItemId, ProjectItemType, Repository, RepositoryId } from "../../../model";
+
+export interface RemoveDeveloperProjectProjectItemParams {}
+
+export interface RemoveDeveloperProjectProjectItemBody {
+  projectItemId: ProjectItemId;
+}
+
+export interface RemoveDeveloperProjectProjectItemQuery {}
+
+export interface RemoveDeveloperProjectProjectItemResponse {}
--- src/dto/onboarding/projectItems/index.ts
+++ src/dto/onboarding/projectItems/index.ts
@@ -1,1 +1,3 @@
 export * from "./UpsertDeveloperProjectItem.dto";
+export * from "./UpsertDevelopperProjectItemService.dto";
+export * from "./RemoveDeveloperProjectItem.dto";
