168eded781e6bd9fb2a2cc33a6f9abebdf288f2b
Lauriane Mollier <lauriane@open-source-economy.com>
2025-08-18 14:36:32 +0200

    WIP

--- src/model/project/ProjectItem.ts
+++ src/model/project/ProjectItem.ts
@@ -1,6 +1,7 @@
 import { UUID } from "../UUID";
 import { ValidationError, Validator } from "../error";
 import { DeveloperProfileId, DeveloperProjectId, DeveloperProjectItem, MergeRights, ProjectRole, ProjectType } from "../onboarding";
+import { OwnerId, RepositoryId } from "../github";
 
 export enum ProjectItemType {
   GITHUB_REPOSITORY = "GITHUB_REPOSITORY",
@@ -12,42 +13,8 @@ export class ProjectItemId extends UUID {}
 
 export interface ProjectItem {
   id: ProjectItemId;
-  projectId: string | null;
   projectItemType: ProjectItemType;
-  githubOwnerId?: number | null; // TODO: improve type
-  githubOwnerLogin?: string | null;
-  githubRepositoryId?: number | null;
-  githubRepositoryName?: string | null;
-  url?: string | null;
+  sourceIdentifier: OwnerId | RepositoryId | string; // string being an url
   createdAt: Date;
   updatedAt: Date;
-}
-
-export namespace ProjectItemCompanion {
-  // export function fromBackend(row: any): DeveloperProjectItem | ValidationError {
-  //     const validator = new Validator(row);
-  //     const id = validator.requiredString("id");
-  //     const developerProfileId = validator.requiredString("developer_profile_id");
-  //     const projectItemId = validator.requiredString("TODO");
-  //     const projectType = validator.requiredEnum("project_type", Object.values(ProjectType) as ProjectType[]);
-  //     const role = validator.requiredEnum("role", Object.values(ProjectRole) as ProjectRole[]);
-  //     const mergeRights = validator.requiredEnum("merge_rights", Object.values(MergeRights) as MergeRights[]);
-  //     const createdAt = validator.requiredDate("created_at");
-  //     const updatedAt = validator.requiredDate("updated_at");
-  //
-  //     const error = validator.getFirstError();
-  //     if (error) {
-  //         return error;
-  //     }
-  //
-  //     return {
-  //         id: new DeveloperProjectId(id),
-  //         developerProfileId: new DeveloperProfileId(developerProfileId),
-  //         projectItemId: new ProjectItemId(projectItemId)
-  //         role,
-  //         mergeRights,
-  //         createdAt,
-  //         updatedAt,
-  //     };
-  // }
 }
