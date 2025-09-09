072ed788e030bbb26909acfb6f6d3e8442980a69
Lauriane Mollier <lauriane@open-source-economy.com>
2025-09-09 03:30:13 +0200

    WIP

--- src/model/project/ServiceType.ts
+++ src/model/project/ServiceType.ts
@@ -13,3 +13,27 @@ export enum Priority {
   HIGH = "High",
   CRITICAL = "Critical",
 }
+
+export enum SupportSubServiceType {
+  BUG_FIXES = "Bug Fixes",
+  NEW_FEATURES = "New Features",
+  CODE_MAINTENANCE = "Code Maintenance",
+}
+
+export enum DevelopmentSubServiceType {
+  TECHNICAL_ASSISTANCE = "Technical Assistance",
+  DEPLOYMENT_GUIDANCE = "Deployment Guidance",
+  CUSTOMER_SUPPORT = "Customer Support",
+}
+
+export enum SecurityAndComplianceSubServiceType {
+  INCIDENT_RESPONSE = "Incident Response",
+  PROACTIVE_MAINTENANCE = "Proactive Monitoring",
+  SUPERVISION = "24/7 Supervision",
+}
+
+export enum AdvisorySubServiceType {
+  ARCHITECTURE_DESIGN = "Architecture Design",
+  TECHNOLOGY_ASSESSMENT = "Technology Assessment",
+  SECURITY_PERFORMANCE = "Security & Performance",
+}
